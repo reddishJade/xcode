10e2b06b209ef5eb3400d50f9257a759920cd985
reddishJade <dwei2615@gmail.com>
2026-06-15 19:24:16 +0800

    chore: remove unused tool validation modules

--- src/xcode/ai/__init__.py
+++ src/xcode/ai/__init__.py
@@ -2,8 +2,6 @@
 
 from .registry import get_model, get_models, get_providers, resolve_model
 from .types import dump_context, load_context
-from .validation import ToolValidationError, validate_tool_call
-
 __all__ = [
     "dump_context",
     "get_model",
@@ -11,6 +9,4 @@ __all__ = [
     "get_providers",
     "load_context",
     "resolve_model",
-    "ToolValidationError",
-    "validate_tool_call",
 ]
